26e52d41f9d98ac9c4e31f226d2072f5d875df4f
Mixilino <nisavicmiha@gmail.com>
2023-10-19 22:09:16 +0200

    add docs around CrystalBallSearch

--- search/crystal_ball.go
+++ search/crystal_ball.go
@@ -4,6 +4,9 @@ import (
 	"math"
 )
 
+// CrystalBallSearch is a search algorithm that jump sqrt(n) times and then linear search
+// Condition is having 2 times max comparison that element from slice is bigger than target
+// This is not really crystal ball search because crystal ball shouldnt receive target, but this is similar algorithm
 func CrystalBallSearch(list []int, target int) int {
 	jump := int(math.Ceil(math.Sqrt(float64(len(list)))))
 	var low, high int
